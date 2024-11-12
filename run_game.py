c43dc6104b2513ad8ad07831250d11f56e725e26
RelativelyJoy001 <oguntim@gmail.com>
2024-11-11 23:32:32 -0500

    the final run game file

---
+++ run_game.py
@@ -0,0 +1,27 @@
+import game
+import random
+
+
+
+with open ("word.txt", "r") as file:
+    content = file.readlines()
+
+def chose_random_word():
+    list_of_words = content
+    word_chosen = random.choice(list_of_words)
+    return word_chosen
+
+while True:
+    random_word = chose_random_word()
+    num_of_guesses = 4 + len(random_word)
+
+    game.play_word_game(random_word, num_of_guesses)
+
+    play_again = input("Do you wish to play again[type 'yes' or 'no']: ")
+
+    if play_again == "yes":
+        continue
+    else:
+        print("Thanks for plkaying!!")
+        break
+    
